fd23f908e98851a84914f820b61fa118d0e4dc1f
blvck237 <jfokoua@gmail.com>
2025-08-12 13:10:16 +0200

    Update google icon

--- src/components/ui/GoogleLoginButton.tsx
+++ src/components/ui/GoogleLoginButton.tsx
@@ -1,4 +1,3 @@
-import { FaGoogle } from 'react-icons/fa';
 import React from 'react';
 
 interface GoogleLoginButtonProps {
@@ -24,7 +23,7 @@ const GoogleLoginButton: React.FC<GoogleLoginButtonProps> = ({
 				disabled={disabled || loading}
 				className="w-full flex justify-center items-center py-3 px-4 border border-gray-300 rounded-lg shadow-sm bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 transition-all duration-200"
 			>
-				<FaGoogle className="h-4 w-4 mr-2 text-red-500" />
+				<img src="/images/google.webp" alt="Google" className="h-6 w-6 mr-2" />
 				{isLogin ? 'Se connecter avec Google' : "S'inscrire avec Google"}
 			</button>
 		</div>
