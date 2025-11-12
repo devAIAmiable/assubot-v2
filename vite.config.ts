d28f1d0a86436c7e5d352a401fdf8fe7b3d17ca2
blvck237 <jfokoua@gmail.com>
2025-11-12 16:15:54 +0100

    ✨ Simplify Vite Configuration by Streamlining Path Alias
    - Updated Vite configuration to simplify the path alias for the `src` directory, replacing the previous resolution method with a direct string reference for improved clarity and maintainability.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,13 @@
-import { dirname, resolve } from 'path';
-
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': '/src',
     },
   },
 });
